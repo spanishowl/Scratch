cb672316a0616ac9fa70071a380e7d9644733231
Vivian Coday <56006735+spanishowl@users.noreply.github.com>
2020-04-27 18:12:12 -0700

    Create MeshGenerator.cs

---
+++ MeshGenerator.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    
+    Vector3[] vertices;
+    int[] triangles;
+	
+	public int xSize = 20;
+	public int zSize = 20;
+	
+	void Start()
+	{
+	    mesh = new Mesh();
+	    GetComponent<MeshFilter>().mesh = mesh;
+	    
+	    CreateShape();
+	    UpdateMesh();
+	}
+	
+	void CreateShape()
+	{
+	    vertices = new Vector3[(xSize + 1) * (zSize +1)];
+	    
+	    for (int i = 0, z = 0; z <= zSize; z++)
+	    {
+	        for (int x = 0; x <= xSize; x++)
+	        {
+	            vertices[i] = new Vector3(x, 0, z);
+	            i++;
+	        }
+	    }
+	}
+	
+	void UpdateMesh()
+	{
+	    mesh.Clear();
+	    
+	    mesh.vertices = vertices;
+	    mesh.triangles = triangles;
+	    
+	    mesh.RecalculateNormals();
+	}
+	
+	private void OnDrawGizmos()
+	{
+	    if (vertices == null)
+	    {
+	        return;
+	    }
+	    
+	    for (int i = 0; i < vertices.Length; i++)
+	    {
+	        Gizmos.DrawSphere(vertices[i], .1f );
+	    }
+	}
+	
+}
